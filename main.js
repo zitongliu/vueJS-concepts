4b96e16dd224f33e6e4e1924cab316565b5ec804
zitongliu <zitongliu7@gmail.com>
2017-08-28 17:26:02 +1000

    added bind directives

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ new Vue({
     },
     addElement: function() {
       this.elements.push(this.elements.length + 1);
+    },
+    getColor: function(number) {
+      return number % 2 === 0 ? 'green' : 'red'
     }
   }
 })
