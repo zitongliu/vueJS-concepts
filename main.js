912bbb9a381b7e174218bc73fbe9630a66c3aa9b
zitongliu <zitongliu7@gmail.com>
2017-08-29 23:22:01 +1000

    added component custom events

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 
 Vue.component('app-username', {
+  props: ['username'],
   data: function() {
     return {
-      username: 'Chris'
+      // username: 'Chris'
     }
   },
-  template: '<p>{{ username }}</p>'
+  template: '<p v-on:click="usernameClicked">{{ username }}</p>',
+  methods: {
+    usernameClicked(){
+      this.$emit('userclicked', this.username);
+    }
+  }
 });
 
 new Vue({
@@ -23,6 +29,9 @@ new Vue({
     },
     getColor: function(number) {
       return number % 2 === 0 ? 'green' : 'red'
+    },
+    userWasClicked(name) {
+      alert(name);
     }
   }
 })
